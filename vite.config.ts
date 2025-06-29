725375ee3170e425708a71d3c154ff5a766df88d
fewbadboy <renekton.king@qq.com>
2025-06-29 16:23:58 +0800

    build: 优化打包配置信息

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,23 @@ import { createSvgIconsPlugin } from 'vite-plugin-svg-icons-ng'
 import tailwindcss from '@tailwindcss/vite'
 import oxlintPlugin from 'vite-plugin-oxlint'
 
+// import { visualizer } from 'rollup-plugin-visualizer'
+
 // https://vite.dev/config/
 export default defineConfig({
   base: '/vue-app/',
+  server: {
+    host: true,
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   plugins: [
     createSvgIconsPlugin({
       iconDirs: [path.resolve(process.cwd(), 'src/icons/svg')],
@@ -26,24 +40,33 @@ export default defineConfig({
     tailwindcss(),
     oxlintPlugin({
       configFile: '.oxlintrc.json',
-      path: 'src'
-    })
+      path: 'src',
+    }),
+    // visualizer({ open: true }),
   ],
   resolve: {
     alias: {
       '@': '/src',
     },
   },
-  server: {
-    host: true,
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('element-plus')) {
+              return 'element-plus'
+            }
+            if (id.includes('vue')) {
+              return 'vue'
+            }
+            if (id.includes('lodash')) {
+              return 'lodash'
+            }
+            return 'vendor'
+          }
+        },
+      },
+    },
   },
 })
